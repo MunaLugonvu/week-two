87630efafcf3c2fdddd6bdf9b2e81d84cd3ef51d
MunaLugonvu <munalugonvu@gmail.com>
2019-11-20 15:36:02 +0300

    update changes

--- js/challenge1.js
+++ js/challenge1.js
@@ -2,14 +2,14 @@ var age;
 age = window.prompt("Please enter your age")
 
 if (age < 18) {
-    alert( "You are a minor!");
+    document.writeln( "You are a minor!");
 
 }
 
 else if ( age > 36) {
-    alert("You are an elder!")
+    document.writeln("You are an elder!")
 }
  else {
-   alert ("You are an adult!")
+   document.writeln ("You are an adult!")
 }
 
