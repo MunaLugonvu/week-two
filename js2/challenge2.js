2a439c5fe161ab2c2b7fc39146f6f0208482e6ca
MunaLugonvu <munalugonvu@gmail.com>
2019-12-09 19:23:09 +0300

    update

--- js2/challenge2.js
+++ js2/challenge2.js
@@ -1,22 +1,23 @@
 
-    const vowels = ['a','e','i','o','u','A','E','I','O','U']; // List of Vowels
-    var vowelsOnly =[];// I  am declaring the array going to store the vowels
-    var duplicateVowels = "";// declaring an empty string going to contain the number of the duplicated vowels
-   
-    function vowelCount(str1){
-        for (const letter of str1) {
-        for (const vowel of vowels) {
-          if (vowel===letter) {
-            if(vowelsOnly.includes(letter)){// checking if vowel already exists in our string
-             ++duplicateVowels
-            }else{
-              vowelsOnly+= letter;
-            }
-            
-          }
-        } 
-          }
-          return ( `('${vowelsOnly}', ${duplicateVowels})`); 
-        }
+const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']; // List of Vowels
+var vowelsOnly = [];// I  am declaring the array going to store the vowels
+var duplicateVowels = "";// declaring an empty string going to contain the number of the duplicated vowels
+function vowelCount(str1) {
+  for (const letter of str1) {
     
-      console.log(vowelCount("Amaaaaazingg"));
+    if (vowels.includes(letter)) {
+      
+        if (vowelsOnly.includes(letter)) {// checking if vowel already exists in our string
+          ++duplicateVowels
+        } else {
+          vowelsOnly += letter;
+        }
+
+      }
+    }
+    return (`('${vowelsOnly}', ${duplicateVowels})`);
+  }
+  
+
+
+console.log(vowelCount("Amaaaaazingg"));
